5c05587ffe1e590fd3215dd479dc51012b2f118f
Cameron <cameron.taylor.ninja@gmail.com>
2017-10-19 20:11:36 -0700

    Initial commit

---
+++ source/PlayState.hx
@@ -0,0 +1,27 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.FlxState;
+import flixel.text.FlxText;
+import flixel.ui.FlxButton;
+import flixel.math.FlxMath;
+
+class PlayState extends FlxState
+{
+	private var _player:Player;
+	
+	override public function create():Void
+	{
+		
+		_player = new Player(20, 20);
+		add(_player);
+		
+		super.create();
+	}
+
+	override public function update(elapsed:Float):Void
+	{
+		super.update(elapsed);
+	}
+}
