a83fb59fc47cf8f9d79e35ec6988add77f083c99
Cameron <cameron.taylor.ninja@gmail.com>
2017-10-23 19:59:36 -0700

    tilemap shiiit lolol

--- assets/data/tilesetWalmart.tsx
+++ assets/data/tilesetWalmart.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="tilesetWalmart" tilewidth="32" tileheight="32" tilecount="450" columns="30">
- <image source="../images/tileset1_2.png" width="960" height="481"/>
+<tileset name="tileset1" tilewidth="32" tileheight="32" tilecount="512" columns="32">
+ <image source="../images/tileset1.png" trans="100e0f" width="1024" height="512"/>
 </tileset>
