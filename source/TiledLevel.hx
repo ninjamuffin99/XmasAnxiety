669f8c33a81bdf4ce5b9f134dd8e4650fe187043
Cameron <cameron.taylor.ninja@gmail.com>
2017-10-24 17:27:04 -0700

    idk with this shit

--- source/TiledLevel.hx
+++ source/TiledLevel.hx
@@ -1,7 +1,18 @@
 package;
 
+import flixel.FlxG;
+import flixel.FlxObject;
+import flixel.FlxSprite;
+import flixel.addons.editors.tiled.TiledImageLayer;
+import flixel.addons.editors.tiled.TiledImageTile;
+import flixel.addons.editors.tiled.TiledLayer.TiledLayerType;
 import flixel.addons.editors.tiled.TiledMap;
 import flixel.addons.editors.tiled.TiledMap.FlxTiledMapAsset;
+import flixel.addons.editors.tiled.TiledObject;
+import flixel.addons.editors.tiled.TiledObjectLayer;
+import flixel.addons.editors.tiled.TiledTileSet;
+import flixel.group.FlxGroup;
+import flixel.tile.FlxTilemap;
 
 /**
  * ...
@@ -10,12 +21,140 @@ import flixel.addons.editors.tiled.TiledMap.FlxTiledMapAsset;
 class TiledLevel extends TiledMap 
 {
 	
-	private inline static var c_PATH_LEVEL_TILESHEETS = "assets/tiled/";
+	private inline static var c_PATH_LEVEL_TILESHEETS = "assets/data/";
+	
+	public var foregroundTiles:FlxGroup;
+	public var objectLayer:FlxGroup;
+	public var backgroundLayer:FlxGroup;
+	private var collidableTIleLayers:Array<FlxTilemap>;
+	
+	public var imagesLayer:FlxGroup;
 
 	public function new(tiledLevel:Dynamic, state:PlayState) 
 	{
 		super(tiledLevel);
 		
+		imagesLayer = new FlxGroup();
+		foregroundTiles = new FlxGroup();
+		objectLayer = new FlxGroup();
+		backgroundLayer = new FlxGroup();
+		
+		FlxG.camera.setScrollBoundsRect(0, 0, fullWidth, fullHeight, true);
+		
+		loadImages();
+		loadObjects(state);
+		
+		for (layer in layers)
+		{
+			if (layer.type != TiledLayerType.TILE) continue;
+		}
+	}
+	
+	public function loadObjects(state:PlayState)
+	{
+		for (layer in layers)
+		{
+			if (layer.type != TiledLayerType.OBJECT)
+				continue;
+			var objectLayer:TiledObjectLayer = cast layer;
+			
+			if (layer.name == "images")
+			{
+				for (o in objectLayer.objects)
+				{
+					loadImageObject(o);
+				}
+			}
+			
+			if (layer.name == "objects")
+			{
+				for (o in objectLayer.objects)
+				{
+					loadObject(state, o, objectLayer, objectsLayer);
+				}
+			}
+		}
+	}
+	
+	private function loadImageObject(object:TiledObject)
+	{
+		var tilesImageCollection:TiledTileSet = this.getTileSet("imageCollection");
+		var tileImageSource:TiledImageTile = tilesImageCollection.getImageSourceByGid(object.gid);
+		
+		var levelsDir:String = "assets/data/";
+		
+		var decoSprite:FlxSprite = new FlxSprite(0, 0, levelsDir + tileImageSource.source);
+		if (decoSprite.width != object.width || decoSprite.height != object.height)
+		{
+			decoSprite.antialiasing = true;
+			decoSprite.setGraphicSize(object.width, object.height);
+		}
+		if (object.flippedHorizontally)
+		{
+			decoSprite.flipX = true;
+		}
+		if (object.flippedVertically)
+		{
+			decoSprite.flipY = true;
+		}
+		decoSprite.setPosition(object.x, object.y - decoSprite.height);
+		decoSprite.origin.set(0, decoSprite.height);
+		if (object.angle != 0)
+		{
+			decoSprite.angle = object.angle;
+			decoSprite.antialiasing = true;
+		}
+		
+		if (object.properties.contains("depth"))
+		{
+			var depth = Std.parseFloat(object.properties.get("depth"));
+			decoSprite.scrollFactor.set(depth, depth);
+		}
+		backgroundLayer.add(decoSprite);
+	}
+	
+	private function loadObject(state:PlayState, o:TiledObject, g:TiledObjectLayer, group:FlxGroup)
+	{
+		var x:Int = o.x;
+		var y:Int = o.y;
+		
+		if (o.gid != 1)
+			y -= g.map.getGidOwner(o.gid).tileHeight;
+		
+		switch (o.type.toLowerCase())
+		{
+			case "player_start":
+				var player = new Player(x, y);
+				FlxG.camera.follow(player);
+				state.player = player;
+				group.add(player);
+			case "floor":
+				var floor = new FlxObject(x, y, o.width, o.height);
+				state.floor = floor;
+			case "coin":
+				var tileset = g.map.getGidOwner(o.gid);
+				var coin = new FlxSprite(x, y, c_PATH_LEVEL_TILESHEETS + tileset.imageSource);
+				state.coins.add(coin);
+			case "exit":
+				var exit = new FlxSprite(x, y);
+				exit.makeGraphic(32, 32);
+				exit.exists = false;
+				state.exit = exit;
+				group.add(exit);
+		}
+	}
+	
+	public function loadImages()
+	{
+		for (layer in layers)
+		{
+			if (layer.type != TiledLayerType.IMAGE)
+				continue;
+			
+			var image:TiledImageLayer = cast layer;
+			var sprite = new FlxSprite(image.x, image.y, c_PATH_LEVEL_TILESHEETS + image.imagePath);
+			imagesLayer.add(sprite);
+		}
 	}
 	
 }
